cd300fc229e542d699550a9773ac3688b432b65b
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-04-30 21:22:36 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -17,6 +17,7 @@ class StudentsController < ApplicationController
     @student.first_name = params[:first_name]
     @student.last_name = params[:last_name]
     Student.create!(first_name: @student.first_name, last_name: @student.last_name )
+    redirect_to student_path
   end
 
 end
